ac0150340d5c675b3547c1d0d31f3b58b85f9013
xiaodong821 <74200168+xiaodong821@users.noreply.github.com>
2020-11-10 22:18:04 +0800

    Update HelloGit.java

--- src/main/java/com/dtdream/HelloGit.java
+++ src/main/java/com/dtdream/HelloGit.java
@@ -3,7 +3,7 @@ package com.dtdream;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git");
-		System.out.println("在线更新");
+		System.out.println("在线更新11");
 	}
 
 }
