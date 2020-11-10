a8976e3bcac82ee0411684bc6a1281f627550c93
xiaodong821 <1451987734@qq.com>
2020-11-10 22:34:00 +0800

    33

--- src/main/java/com/dtdream/HelloGit.java
+++ src/main/java/com/dtdream/HelloGit.java
@@ -3,7 +3,7 @@ package com.dtdream;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git");
-		System.out.println("1222111");
+		System.out.println("122Æä31");
 		System.out.println("33331111");
 		System.out.println("ÔÚÏß11");
 	}
